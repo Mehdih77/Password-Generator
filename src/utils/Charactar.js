b1ce865f8a423ee55a3df93bd788ff21b632926b
Mehdih77 <mehdiwork77@gmail.com>
2021-08-05 11:26:02 +0430

    start password generator project

---
+++ src/utils/Charactar.js
@@ -0,0 +1,4 @@
+export const UpperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const LowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+export const Numbers = '0123456789';
+export const Symbols = '!@#$%^&*()-=+;<>?|][_';
